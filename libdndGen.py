b37fd7fb59e9689fc6c46192738f9a0f7b6cf76b
Tyler Dinsmoor <pappad@airmail.cc>
2015-05-09 22:11:04 +0900

    updated copyright info

--- libdndGen.py
+++ libdndGen.py
@@ -3,7 +3,7 @@
 #
 #  libdndGen.py
 #
-#  Copyright 2014 Tyler Dinsmoor <d@d-netbook>
+#  Copyright 2014 Tyler Dinsmoor <pappad@airmail.cc>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
