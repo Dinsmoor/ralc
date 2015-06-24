30993a48e69c790bb69bd092fd8b8271dbfd4d4f
Tyler Dinsmoor <pappad@airmail.cc>
2015-06-25 00:07:55 +0900

    Removed unneeded methods from UI.
    Moved all dialogs to dialogs.py.
    
    Removed unneeded tier from treeview.

---
+++ dialogs.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  dialogs.py
+#
+#  Copyright 2015 Tyler Dinsmoor <pappad@airmail.cc>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+
+# Master Dialog modified for grid (tkSimpleDialog)
+import Tkinter as tk
+import ttk
+import tkMessageBox
+import os
+import shutil
+
+class Dialog(tk.Toplevel):
+
+	def __init__(self, parent, title = None):
+
+		tk.Toplevel.__init__(self, parent)
+		self.transient(parent)
+		if title:
+			self.title(title)
+		self.parent = parent
+		self.result = None
+		body = tk.Frame(self)
+		self.initial_focus = self.body(body)
+		body.grid(padx=5, pady=5)
+		self.buttonbox()
+		self.grab_set()
+		if not self.initial_focus:
+			self.initial_focus = self
+		self.protocol("WM_DELETE_WINDOW", self.cancel)
+		self.geometry("+%d+%d" % (parent.winfo_rootx()+50,
+								  parent.winfo_rooty()+50))
+		self.initial_focus.focus_set()
+		self.wait_window(self)
+
+	def body(self, master):
+		pass
+
+	def buttonbox(self):
+		box = tk.Frame(self)
+
+		w = tk.Button(box, text="OK", width=10, command=self.ok, default='active')
+		w.grid(row=0,column=0, padx=5, pady=5)
+		w = tk.Button(box, text="Cancel", width=10, command=self.cancel)
+		w.grid(row=0,column=1, padx=5, pady=5)
+
+		self.bind("<Return>", self.ok)
+		self.bind("<Escape>", self.cancel)
+
+		box.grid(row=2)
+
+	def ok(self, event=None):
+		if not self.validate():
+			self.initial_focus.focus_set()
+			return
+		self.withdraw()
+		self.update_idletasks()
+		self.apply()
+		self.cancel()
+
+	def cancel(self, event=None):
+		self.parent.focus_set()
+		self.destroy()
+
+	def validate(self):
+		return 1
+
+	def apply(self):
+		pass
+
+
+
+class CharSheetGen(Dialog):
+
+	def body(self, master):
+
+		'''
+		Just a text box for displaying generated sheet.
+		Should use pages later to specify options for a custom sheet.
+		'''
+
+		self.title('Character Sheet Generator')
+		self.char_gen_tabs = ttk.Notebook(self)
+		self.char_gen_tabs.grid()
+
+		self.show_page = tk.Frame(self)
+		self.set_page = tk.Frame(self)
+
+		self.char_gen_tabs.add(self.show_page, text='Sheet')
+		self.char_gen_tabs.add(self.set_page, text='Settings')
+
+		tk.Label(self.show_page, text="Generate a random character sheet.").grid(row=0, columnspan=2)
+		self.char_text = tk.Text(self.show_page, width=50,
+			height=30, state='disabled')
+		self.char_text.grid(row=1, column=0)
+
+		self.apply()
+
+	def buttonbox(self):
+		box = tk.Frame(self)
+
+		w = tk.Button(box, text="Next", width=10, command=self.ok, default='active')
+		w.grid(row=0,column=0, padx=5, pady=5)
+		w = tk.Button(box, text="Close", width=10, command=self.cancel)
+		w.grid(row=0,column=1, padx=5, pady=5)
+
+		self.bind("<Return>", self.ok)
+		self.bind("<Escape>", self.cancel)
+
+		box.grid(row=2)
+
+	def ok(self, event=None):
+		'''
+		Need to replace stock ok() because I don't want it to exit
+		immediately after creating a new sheet.
+		'''
+
+		self.apply()
+
+	def apply(self):
+		char_sheet = charGen.custom_param(self.parent.settings)
+		self.char_text.config(state='normal')
+		self.char_text.delete(1.0, 'end')
+		self.char_text.insert('end',char_sheet['Info'])
+		self.char_text.config(state='disabled')
+
+
+
+class SaveDialog(Dialog):
+
+	def body(self, master):
+
+		self.title('Save')
+		l1 = tk.Label(master, text="Choose a name to save your area.")
+		l1.grid(row=0, columnspan=2)
+		l2 = tk.Label(master, text="Save Name:")
+		l2.grid(row=1)
+		self.e1 = tk.Entry(master)
+		self.e1.grid(row=1, column=1)
+		try:
+			self.e1.insert(0, self.parent.cityName)
+			return self.e1
+		except:
+			tkMessageBox.showerror('Error','No data to save!')
+			self.cancel()
+
+	def apply(self):
+
+		save_name = str(self.e1.get())
+		self.parent.save_all(save_name)
+
+class SettingsMenu(Dialog):
+
+	def body(self, master):
+		self.title('Settings')
+		self.settings_tabs = ttk.Notebook(self)
+		self.settings_tabs.grid()
+
+		self.data_dict = dict()
+
+		self.main_page = tk.Frame(self)
+		self.map_page = tk.Frame(self)
+		self.town_page = tk.Frame(self)
+		self.char_page = tk.Frame(self)
+
+		self.settings_tabs.add(self.main_page,text="Main")
+		self.settings_tabs.add(self.map_page,text="Map")
+		self.settings_tabs.add(self.town_page,text="Towns")
+		self.settings_tabs.add(self.char_page,text="Chars")
+
+		def make_main_page():
+			self.use_img = tk.BooleanVar()
+			ttk.Checkbutton(self.main_page, variable=self.use_img,
+				text="Use Generated Images").grid()
+
+		def make_map_page():
+			tk.Label(self.map_page, text="Town Size").grid(row=0, column=0)
+			self.data_dict['city_size_scale'] = tk.Scale(self.map_page,
+				orient='horizontal', from_=0, to=20).grid(row=0, column=1)
+
+		def make_town_page():
+			pass
+
+		def make_char_page():
+			tk.Label(self.char_page, text="Median Char Level").grid(row=0, column=0)
+			self.data_dict['char_level'] = tk.Scale(self.char_page,
+				orient='horizontal', from_=1, to=30).grid(row=0, column=1)
+
+		make_main_page()
+		make_map_page()
+		make_town_page()
+		make_char_page()
+
+	def apply(self):
+		#print self.data_dict['city_size_scale'].get()
+		print self.use_img.get()
+		return self.data_dict
+
+class LoadDialog(Dialog):
+
+	def body(self, master):
+
+		self.title('Load')
+		self.file_listbox = tk.Listbox(master, selectmode='single')
+		self.get_save_dir()
+		tk.Label(master,
+			text="Choose a name to load your area.").grid(row=0,
+				columnspan=2)
+		self.file_listbox.grid(row=1, columnspan=2)
+		self.del_opt = bool()
+		self.del_cb = tk.Button(master, text="Delete",
+			command=self.delete_save).grid(row=2, columnspan=2)
+
+	def get_save_dir(self):
+
+		self.file_listbox.delete(0, 'end')
+		dir_list = os.listdir('save')
+		for subfolder in dir_list:
+			self.file_listbox.insert('end', subfolder)
+		print "LoadDialog.get_save_dir.Done"
+
+	def delete_save(self):
+
+		selected = self.file_listbox.curselection()
+		del_name = str(self.file_listbox.get(selected))
+		try:
+			shutil.rmtree('save/%s'%del_name)
+			print "LoadDialog.delete_save.Done"
+		except OSError:
+			print "LoadDialog.delete_save.OSError['Item not a Dir?']"
+		self.get_save_dir()
+
+
+	def apply(self):
+		try:
+			selected = self.file_listbox.curselection()
+			load_name = str(self.file_listbox.get(selected))
+			self.parent.load_all(load_name)
+		except:
+			tkMessageBox.showerror(
+			"Error","Problem loading save!\nSave from old version?")
+
--- main.py
+++ main.py
@@ -26,6 +26,7 @@ try:
 	import Tkinter as tk
 	import tkMessageBox
 	import ttk
+	import dialogs
 	import mapGen
 	import charGen
 	import collections as col
@@ -33,72 +34,12 @@ try:
 	import cPickle as pickle
 	import itemGen
 	import os
-	import shutil
 except ImportError:
 	tkMessageBox.showerror(
 		"Error","You are missing essential Libraries. See README.md")
 	print "You are missing essential Libraries. See README.md"
 	exit()
 
-# Master Dialog modified for grid (tkSimpleDialog)
-
-class Dialog(tk.Toplevel):
-
-	def __init__(self, parent, title = None):
-
-		tk.Toplevel.__init__(self, parent)
-		self.transient(parent)
-		if title:
-			self.title(title)
-		self.parent = parent
-		self.result = None
-		body = tk.Frame(self)
-		self.initial_focus = self.body(body)
-		body.grid(padx=5, pady=5)
-		self.buttonbox()
-		self.grab_set()
-		if not self.initial_focus:
-			self.initial_focus = self
-		self.protocol("WM_DELETE_WINDOW", self.cancel)
-		self.geometry("+%d+%d" % (parent.winfo_rootx()+50,
-								  parent.winfo_rooty()+50))
-		self.initial_focus.focus_set()
-		self.wait_window(self)
-
-	def body(self, master):
-		pass
-
-	def buttonbox(self):
-		box = tk.Frame(self)
-
-		w = tk.Button(box, text="OK", width=10, command=self.ok, default='active')
-		w.grid(row=0,column=0, padx=5, pady=5)
-		w = tk.Button(box, text="Cancel", width=10, command=self.cancel)
-		w.grid(row=0,column=1, padx=5, pady=5)
-
-		self.bind("<Return>", self.ok)
-		self.bind("<Escape>", self.cancel)
-
-		box.grid(row=2)
-
-	def ok(self, event=None):
-		if not self.validate():
-			self.initial_focus.focus_set()
-			return
-		self.withdraw()
-		self.update_idletasks()
-		self.apply()
-		self.cancel()
-
-	def cancel(self, event=None):
-		self.parent.focus_set()
-		self.destroy()
-
-	def validate(self):
-		return 1
-
-	def apply(self):
-		pass
 
 ####################
 
@@ -359,11 +300,11 @@ https://www.gnu.org/licenses/gpl-2.0.html
 		Interior handler for an external class
 		'''
 
-		LoadDialog(self)
+		dialogs.LoadDialog(self)
 
 	def create_settings_menu(self):
 
-		SettingsMenu(self)
+		dialogs.SettingsMenu(self)
 
 	def create_save_dialog(self):
 
@@ -371,7 +312,7 @@ https://www.gnu.org/licenses/gpl-2.0.html
 		Interior handler for an external class
 		'''
 
-		SaveDialog(self)
+		dialogs.SaveDialog(self)
 
 	def create_char_sheet_dialog(self):
 
@@ -379,7 +320,7 @@ https://www.gnu.org/licenses/gpl-2.0.html
 		Interior handler for an external class
 		'''
 
-		CharSheetGen(self)
+		dialogs.CharSheetGen(self)
 
 	def load_state(self):
 
@@ -391,17 +332,6 @@ https://www.gnu.org/licenses/gpl-2.0.html
 		self.create_widgets()
 		self.get_photo()
 		print 'UI.load_state.Done'
-
-	def new_state(self):
-
-		'''
-		Just calles for new data and redraws widgets.
-		'''
-
-		self.new_data()
-		self.create_widgets()
-		self.get_photo()
-		print 'UI.new_state.Done'
 
 	def get_photo(self):
 
@@ -464,11 +394,6 @@ https://www.gnu.org/licenses/gpl-2.0.html
 								'end', text=bldg['Purpose'])
 							for rooms, roomsdata in bldg.iteritems():
 								# add list of rooms
-								if rooms != 'Purpose':
-									rooms_parent = self.tree.insert(bldg_parent,
-										'end', text=rooms)
-									#bldg_item_parent = self.tree.insert(bldg_parent,
-									#	'end', text='Quests')
 								if type(roomsdata) == list:
 									for room in roomsdata: # list
 										# ensure that the room type gets parsed first
@@ -484,7 +409,7 @@ https://www.gnu.org/licenses/gpl-2.0.html
 										for key, value in room.iteritems(): # dict
 											if key == 'Type':
 												# add the rooms themselves
-												room_parent = self.tree.insert(rooms_parent,
+												room_parent = self.tree.insert(bldg_parent,
 													'end', text=value)
 
 											if key == 'Actors':
@@ -629,173 +554,6 @@ Distance:	%skm to %s.
 			if x_true and y_true:
 				self.tree.selection_set(city_id)
 				self.tree.focus(city_id)
-
-#####
-# Dialogs
-#####
-
-class CharSheetGen(Dialog):
-
-	def body(self, master):
-
-		'''
-		Just a text box for displaying generated sheet.
-		Should use pages later to specify options for a custom sheet.
-		'''
-
-		self.title('Character Sheet Generator')
-		self.char_gen_tabs = ttk.Notebook(self)
-		self.char_gen_tabs.grid()
-
-		self.show_page = tk.Frame(self)
-		self.set_page = tk.Frame(self)
-
-		self.char_gen_tabs.add(self.show_page, text='Sheet')
-		self.char_gen_tabs.add(self.set_page, text='Settings')
-
-		tk.Label(self.show_page, text="Generate a random character sheet.").grid(row=0, columnspan=2)
-		self.char_text = tk.Text(self.show_page, width=50,
-			height=30, state='disabled')
-		self.char_text.grid(row=1, column=0)
-
-		self.apply()
-
-	def buttonbox(self):
-		box = tk.Frame(self)
-
-		w = tk.Button(box, text="Next", width=10, command=self.ok, default='active')
-		w.grid(row=0,column=0, padx=5, pady=5)
-		w = tk.Button(box, text="Close", width=10, command=self.cancel)
-		w.grid(row=0,column=1, padx=5, pady=5)
-
-		self.bind("<Return>", self.ok)
-		self.bind("<Escape>", self.cancel)
-
-		box.grid(row=2)
-
-	def ok(self, event=None):
-		'''
-		Need to replace stock ok() because I don't want it to exit
-		immediately after creating a new sheet.
-		'''
-
-		self.apply()
-
-	def apply(self):
-		char_sheet = charGen.custom_param(ui.settings)
-		self.char_text.config(state='normal')
-		self.char_text.delete(1.0, 'end')
-		self.char_text.insert('end',char_sheet['Info'])
-		self.char_text.config(state='disabled')
-
-
-
-class SaveDialog(Dialog):
-
-	def body(self, master):
-
-		self.title('Save')
-		tk.Label(master, text="Choose a name to save your area.").grid(row=0, columnspan=2)
-		tk.Label(master, text="Save Name:").grid(row=1)
-		self.e1 = tk.Entry(master)
-		self.e1.grid(row=1, column=1)
-		return self.e1
-
-	def apply(self):
-
-		save_name = str(self.e1.get())
-		ui.save_all(save_name)
-
-class SettingsMenu(Dialog):
-
-	def body(self, master):
-		self.title('Settings')
-		self.settings_tabs = ttk.Notebook(self)
-		self.settings_tabs.grid()
-
-		self.data_dict = dict()
-
-		self.main_page = tk.Frame(self)
-		self.map_page = tk.Frame(self)
-		self.town_page = tk.Frame(self)
-		self.char_page = tk.Frame(self)
-
-		self.settings_tabs.add(self.main_page,text="Main")
-		self.settings_tabs.add(self.map_page,text="Map")
-		self.settings_tabs.add(self.town_page,text="Towns")
-		self.settings_tabs.add(self.char_page,text="Chars")
-
-		def make_main_page():
-			self.use_img = tk.BooleanVar()
-			ttk.Checkbutton(self.main_page, variable=self.use_img,
-				text="Use Generated Images").grid()
-
-		def make_map_page():
-			tk.Label(self.map_page, text="Town Size").grid(row=0, column=0)
-			self.data_dict['city_size_scale'] = tk.Scale(self.map_page,
-				orient='horizontal', from_=0, to=20).grid(row=0, column=1)
-
-		def make_town_page():
-			pass
-
-		def make_char_page():
-			tk.Label(self.char_page, text="Median Char Level").grid(row=0, column=0)
-			self.data_dict['char_level'] = tk.Scale(self.char_page,
-				orient='horizontal', from_=1, to=30).grid(row=0, column=1)
-
-		make_main_page()
-		make_map_page()
-		make_town_page()
-		make_char_page()
-
-	def apply(self):
-		#print self.data_dict['city_size_scale'].get()
-		print self.use_img.get()
-		return self.data_dict
-
-class LoadDialog(Dialog):
-
-	def body(self, master):
-
-		self.title('Load')
-		self.file_listbox = tk.Listbox(master, selectmode='single')
-		self.get_save_dir()
-		tk.Label(master,
-			text="Choose a name to load your area.").grid(row=0,
-				columnspan=2)
-		self.file_listbox.grid(row=1, columnspan=2)
-		self.del_opt = bool()
-		self.del_cb = tk.Button(master, text="Delete",
-			command=self.delete_save).grid(row=2, columnspan=2)
-
-	def get_save_dir(self):
-
-		self.file_listbox.delete(0, 'end')
-		dir_list = os.listdir('save')
-		for subfolder in dir_list:
-			self.file_listbox.insert('end', subfolder)
-		print "LoadDialog.get_save_dir.Done"
-
-	def delete_save(self):
-
-		selected = self.file_listbox.curselection()
-		del_name = str(self.file_listbox.get(selected))
-		try:
-			shutil.rmtree('save/%s'%del_name)
-			print "LoadDialog.delete_save.Done"
-		except OSError:
-			print "LoadDialog.delete_save.OSError['Item not a Dir?']"
-		self.get_save_dir()
-
-
-	def apply(self):
-		try:
-			selected = self.file_listbox.curselection()
-			load_name = str(self.file_listbox.get(selected))
-			ui.load_all(load_name)
-		except:
-			tkMessageBox.showerror(
-		"Error","There is no save to load!")
 
 ui = UI()
 ui.mainloop()
