88ce118e276922936516468b793eaa246ce24705
Tyler Dinsmoor <pappad@airmail.cc>
2015-08-20 23:34:25 +0900

    Added economy/govt background info and like

--- townGen.py
+++ townGen.py
@@ -59,24 +59,50 @@ def wChoice(wCh):
             return c
         upto += w
     assert False, "Shouldn't get here"
+
 def getStreetName():
     import random
     streetName = random.choice(getFromFile_T('data/streets'))
     for s in streetName:
         return s
 
+def load_products():
+    proddata = getFromFile_LoL('data/products')
+
+    prod_dict = dict()
+    for prod in proddata:
+        prod_dict[prod[0]] = float(prod[1])
+    return prod_dict
+
+def load_resources():
+    resdata = getFromFile_LoL('data/natural_resources')
+
+    res_dict = dict()
+    for res in resdata:
+        res_dict[res[0]] = float(res[1])
+    return res_dict
+
 class Settlement(object):
     def __init__(self, biome, pref):
         self.settings = pref
-        affluence = self.get_affluence()
+        self.nat_res = self.settings['town']['resources']
+        affluence = self.get_affluence(biome)
         govt = self.get_govt(affluence)
         econ_system = self.get_economy(affluence, govt)
-        #senses = self.get_senses(affluence, govt)
-        citySize = self.getCitySize(biome)
-        self.s = self.getStreets(citySize, biome)
+        laws = self.get_laws(govt)
+        (self.total_utility,
+            self.exports,
+            self.imports) = self.get_wares(affluence, econ_system)
 
-    def parse_settings(self):
-        self.settings['town']['size_mod']
+        #senses = self.get_senses(affluence, govt)
+
+        desc = self.get_description()
+        citySize = self.getCitySize(biome)
+        streets = self.getStreets(citySize, biome)
+
+        self.dat = streets, desc
+
+
 
     def getBldgData(self, biome):
         return bldgGen.main('town', biome, self.settings)
@@ -99,7 +125,7 @@ class Settlement(object):
         return int(mult * (random.randint(1,4) + 10))
 
     def get_govt(self, affluence):
-        
+
         political_systems = {
             0.8:'Anarchism',
             1.2:'City-state',
@@ -115,37 +141,119 @@ class Settlement(object):
 
         power_structure = None
         power_source = None
-        
+
         closest_num = min(political_systems.keys(), key=lambda x:abs(x-affluence))
         return political_systems[closest_num]
 
-    def get_affluence(self):
+    def get_affluence(self, biome):
         aff = self.settings['town']['affluence']
-        
-        #use bonus
+
+        aff += random.randint(5,50)
+        #luck-based region economics?
         if random.random() >= 0.5:
             #positive
             if random.random() >= 0.5:
-                aff += random.random()
+                aff += random.random() * 10
             else:
-                aff -= random.random()
-        
-        if aff <= 0.0:
-            aff = 0.1
-        
+                aff -= random.random() * 5
+
+        resources = load_resources()
+        for res, util in resources.iteritems():
+            if res in self.nat_res:
+                aff += util
+
+        biome_bonus = {
+            'forest':5.0,
+            'plains':2.7,
+            'hills':2.3,
+            'tundra':1.3,
+            'marsh':1.0,
+            'desert':1.5,
+            'small':1.0,
+            }
+        aff *= biome_bonus[biome]
         return aff
 
     def get_clans(self):
         pass
 
     def get_laws(self, govt):
-        pass
-        
-    def get_wares(self, affluence):
-        pass
-    
+        laws = list()
+        law_li = [
+            #Society
+            "Labour law",
+            "Human rights",
+            "Civil procedure",
+            "Evidence",
+            "Immigration law",
+            "Social security",
+            "Family law",
+            "Transactional law",
+            #Commerce
+            "Company law",
+            "Commercial law",
+            "Admirality law",
+            "Intellectual property",
+            "Restitution",
+            "Unjust enrichment",
+            # Regulartory
+            "Tax law",
+            "Banking law",
+            "Utility law",
+            "Competition law",
+            "Consumer law",
+            "Environmental law",
+            ]
+
+        law_strengths = ["weak","restrictive","permissive","undefined"]
+
+        for law in law_li:
+            strength = random.choice(law_strengths)
+            laws.append(law + ": "+strength)
+
+        return laws
+
+
+
+    def get_wares(self, utility_minimum, econ_sys):
+        products = load_products()
+
+        exports = list()
+        imports = list()
+        total_utility = utility_minimum
+
+
+        for product, utility in products.iteritems():
+            if utility <= utility_minimum:
+                exports.append(product)
+                # exports are initially valuable
+                total_utility += utility
+            else:
+                imports.append(product)
+                # imports are good for trade, but not for economy
+                total_utility += utility / 10
+
+        #lets remove some imports/exports depending on type of market
+        # econ_sys: pop_exports, pop_imports
+        economic_systems = {
+            'Anarchist'     :(0,0),
+            'Capitalist'    :(0,0),
+            'Communist'     :(0,random.randint(1,4)),
+            'Corporatist'   :(0,0),
+            'Georgist'      :(4,0),
+            'Laissez-faire' :(1,0),
+            'Market socialist':(0,0),
+            'Mercantilist'  :(0,0),
+            'Participatory' :(0,0),
+            'Protectionist' :(len(exports),len(imports)),
+            'State capitalist':(0,0),
+            }
+
+        return total_utility, exports, imports
+
+
     def get_economy(self, affluence, govt):
-        
+
         political_systems = {
             'Anarchism':('Anarchist','Corporatist','Georgist'),
             'City-state':('Capitalist','Corporatist','Participatory','Mercantilist'),
@@ -158,7 +266,11 @@ class Settlement(object):
             'Monarchy':('Protectionist','Mercantilist'),
             'Theocracy':('Communist','Georgist','Market socialist'),
             }
-        
+
+        ec_sys = random.choice(political_systems[govt])
+        return ec_sys
+
+    def get_description(self):
         # Took descriptions from child articles from https://en.wikipedia.org/wiki/Economic_system
         economic_systems = {
             'Anarchist'     :"The system of specialization in the various crafts, which would lead to a man's following the task for which he had the greatest aptitude, and distributing his surplus products to whoever may need them, receiving what he himself needs of other things from the surplus produced by his neighbours, but always on the basis of free distribution, not of exchange.",
@@ -173,9 +285,11 @@ class Settlement(object):
             'Protectionist' :"The economic policy of restraining trade between states (countries) through methods such as tariffs on imported goods, restrictive quotas, and a variety of other government regulations designed to allow (according to proponents) fair competition between imports and goods and services produced domestically.",
             'State capitalist':"Commercial economic activity is undertaken by the state, where the means of production are organized and managed as business enterprises, including the processes of capital accumulation, wage labor, and centralized management.",
             }
-            
-    def get_description(self):
-        pass
+
+
+        #print 'TotUtil',self.total_utility
+        #print 'EX:',self.exports + self.nat_res
+        #print 'IN:',self.imports
 
     def get_senses(self, affluence, govt):
 
@@ -202,7 +316,6 @@ class Settlement(object):
             random.choice(senses_filtered[4]),
             random.choice(senses_filtered[5]))
         desc = smell_des+sight_des+sound_des
-        print desc
         return desc
 
     def getStreets(self, citySize, biome):
@@ -219,14 +332,21 @@ class Settlement(object):
         return bldgList
 
 def main(opt, biome, pref):
+    biomes = ('forest',
+            'plains',
+            'hills',
+            'tundra',
+            'marsh',
+            'desert',
+            'small')
     if opt == 'big':
         town = Settlement(biome, pref)
-        return town.s
+        return town.dat
     if opt == 'small':
         town = Settlement('small', pref)
-        return town.s
+        return town.dat
     else:
-        town = Settlement('forest', pref)
+        town = Settlement(random.choice(biomes), pref)
         return 0
 
 if __name__ == '__main__':
